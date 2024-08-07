35aa7e03ad3297c1ea8289ad8d8bf7a42de00846
Lakshit Bhardwaj <lakshit.lnmiit@gmail.com>
2024-08-07 12:44:43 +0000

    20240807

--- exercise.py
+++ exercise.py
@@ -1,6 +1,5 @@
 lottery_numbers = {13, 21, 22, 5, 8}
 
-
 players = [ {
     'name': 'Lakshit',
     'numbers': {1, 2, 3, 4, 5}
@@ -10,8 +9,8 @@ players = [ {
 }
     ]
 
-#for i in players:
- #   print(f"Player name {i['name']} has got {len(i['numbers'].intersection(lottery_numbers))} numbers right")
+for i in players:
+    print(f"Player name {i['name']} has got {len(i['numbers'].intersection(lottery_numbers))} numbers right")
 
 d={13, 14, 15, 16, 17}
 
@@ -23,7 +22,7 @@ players = [
 ]
 
 top_player=players[0]
-print(top_player)
+#print(top_player)
 for i in players:
     if len(i['numbers'].intersection(d)) > len(top_player['numbers'].intersection(d)):
         top_player=i
